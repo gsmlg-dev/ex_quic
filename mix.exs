bb81d35b1a921ca61947f7054c84c28442f0d8fe
Jonathan Gao <gsmlg.com@gmail.com>
2026-09-25 08:04:19 +0800

    chore(release): v0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExQuic.MixProject do
   def project do
     [
       app: :ex_quic,
-      version: "0.1.0-dev",
+      version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
